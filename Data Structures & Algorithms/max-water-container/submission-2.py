d75f0be94e834f1df2a04af20ab3100a58795995
Tanjid <tanjid.edx@gmail.com>
2026-04-14 13:48:48 +0600

    Add: max-water-container - submission-2

---
+++ Data Structures & Algorithms/max-water-container/submission-2.py
@@ -0,0 +1,48 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+
+        def findMax(heights):
+            max = 0
+            for i in range(len(heights)-1):
+                far = len(heights) - 1
+                con1 = calCon(heights[i], heights[far], (far-i))
+                far -= 1
+                con2 = 0
+                while far > i:
+                    con = calCon(heights[i], heights[far], (far-i))
+                    
+                    if con > con2:
+                        con2 = con
+
+                    if heights[far] >= heights[i]:
+                        break
+                    
+                    far -= 1
+                
+                lar = 0
+                if con1 > con2:
+                    lar = con1
+                else:
+                    lar = con2
+                
+                if lar > max:
+                    max = lar 
+            
+            return max
+
+        
+
+        def calCon(right, left, distance):
+            if left <= right:
+                return left * distance
+            else:
+                return right * distance 
+
+
+
+        return findMax(heights)
+            
+
+
+
+        
