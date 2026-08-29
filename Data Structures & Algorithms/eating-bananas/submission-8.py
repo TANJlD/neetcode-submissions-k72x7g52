b8d8ad37db043438d52e2f0e2a45374807b9e0d5
Tanjid <tanjid.edx@gmail.com>
2026-08-29 12:48:13 +0600

    Add: eating-bananas - submission-8

---
+++ Data Structures & Algorithms/eating-bananas/submission-8.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        slow = 1
+        fast = max(piles)
+        min_speed = fast
+
+        while slow <= fast:
+            m = (slow + fast) // 2
+            hours = self.countHours(piles, m)
+            if hours > h:
+                slow = m + 1
+            else:
+                fast = m - 1
+                min_speed = min(min_speed, m)
+        
+        return min_speed
+
+
+
+    def countHours(self, piles, speed):
+        hours = 0
+
+        for bananas in piles:
+            if (bananas % speed) != 0:
+                hours += (bananas // speed) + 1
+            else:
+                hours += bananas // speed
+
+        return hours
